26ad87e0fef6d7781d61edd2b80fc2c632cd1787
krotla <kkornel@o2.pl>
2025-07-22 23:42:34 +0200

    feat: Prepare messages list. Add verbose mode.

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 # PROMPT = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
 def main():
     cmd_args = sys.argv
-    if len(cmd_args) <= 1:
+    arg_count = len(cmd_args)
+    if arg_count <= 1:
         print("Program needs prompt as an argument! Exit immediately!")
         sys.exit(1)
+    verbose = True  if arg_count == 3 and cmd_args[2] == "--verbose" else False       
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=cmd_args[1])]),
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=cmd_args[1]
+        model='gemini-2.0-flash-001', contents=messages
     )
-    print(response.text + "\n")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {cmd_args[1]}\n")
+    print(response.text)
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
 
 main()
