2bce5042ba86f54d9e1b8d329c4fded2ce72b491
krotla <kkornel@o2.pl>
2025-07-28 10:07:10 +0200

    feat: Add fedback loop to make LLM iterate to finish task

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def main():
         sys.exit(1)
     verbose = True  if arg_count == 3 and cmd_args[2] == "--verbose" else False       
     
+    if verbose:
+        print(f"User prompt: {cmd_args[1]}\n")
     
     messages = [
         types.Content(role="user", parts=[types.Part(text=cmd_args[1])]),
@@ -39,33 +41,42 @@ def main():
     system_prompt = """
     You are a helpful AI coding agent.
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    When a user asks a question or makes a request, make a function call plan. 
+    You can perform the following operations:
 
     - List files and directories
     - Read file contents
     - Execute Python files with optional arguments
     - Write or overwrite files
 
-    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    All paths you provide should be relative to the working directory, which contains calculator app source code.
+    You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
     conf = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages, config=conf,
-    )
-
-    if verbose:
-        print(f"User prompt: {cmd_args[1]}\n")
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            function_call_result = call_function(function_call_part, verbose)
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception("Fatal ERROR! No result from called function.")
+    for i in range(20):
+        try:
+            response = client.models.generate_content(
+                model='gemini-2.0-flash-001', contents=messages, config=conf,
+            )            
+            if response.candidates:
+                for candidate in response.candidates:
+                    messages.append(candidate.content)
+            if response.function_calls:
+                for function_call_part in response.function_calls:
+                    function_call_result = call_function(function_call_part, verbose)
+                    function_response = function_call_result.parts[0].function_response.response
+                    if not function_response:
+                        raise Exception("Fatal ERROR! No result from called function.")
+                    if verbose:
+                        print(f"-> {function_response}")
+                    messages.append(types.Content(role="user", parts=[types.Part(text=function_response['result'])]))
+            else:
+                print(response.text)
+                break
             if verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
-    if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+        except Exception as e:
+            print(f"Error: {e}")
 
 main()
